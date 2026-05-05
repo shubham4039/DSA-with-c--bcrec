397a7fcd9bc7c18ad3e5c7184fce8dd17c9fc359
shubham4039 <143273628+shubham4039@users.noreply.github.com>
2026-05-05 16:10:10 +0530

    Time: 4 ms (31.84%), Space: 8.6 MB (65%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x<0 || (x%10==0 && x!=0)){
+            return false;
+        }
+        int rev=0, num=x;
+        while(x!=0){
+            if(rev>INT_MAX/10 || rev<INT_MIN/10){
+                return false;
+            }
+            rev=rev*10 + x%10;
+            x/=10;
+        }
+        return num==rev || num==rev/10;    
+    }
+};
